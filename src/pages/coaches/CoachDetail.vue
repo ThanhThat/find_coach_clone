7d8b06def59785e6ca8a92940b0822cf20e86c0d
ThanhThat <thachthanhthat1995@gmail.com>
2023-09-23 00:11:43 +0700

    Feature: thread router.

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Coach Detail</h1>
+    <router-link to="/coaches/c1/contact">Contact</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <h1>Coach List</h1>
-  </div>
+  <section>
+    <section>Filter</section>
+    <section>
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as Coach</router-link>
+      </div>
+      <ul>
+        List of Coaches
+      </ul>
+    </section>
+  </section>
 </template>
 
 <script>
